d08be224bd0866920899fd21ca810438da68b8dc
Tejvir90 <47678473+Tejvir90@users.noreply.github.com>
2020-03-22 01:02:09 -0400

    the code for @ComponentScan and @Configuration- No xml required

---
+++ src/main/java/com/tej/annotationspringdemo/AnnotationStart.java
@@ -0,0 +1,18 @@
+package com.tej.annotationspringdemo;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class AnnotationStart {
+
+
+    public static void main(String args[]) {
+        AnnotationConfigApplicationContext annotationConfigApplicationContext =
+                new AnnotationConfigApplicationContext(SportsConfig.class);
+
+        Coach cricketCoach = annotationConfigApplicationContext.getBean("cricketCoach", Coach.class);
+
+        System.out.println(cricketCoach.getDailyWorkout());
+
+    }
+
+}
